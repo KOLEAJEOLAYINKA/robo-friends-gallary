8d8949f60e9af7328a69ef8b618a7773aeffb9c6
KOLEAJEOLAYINKA <koleajeolayinka@gmail.com>
2022-12-17 06:49:18 +0100

    update robofriend page

---
+++ src/components/cardList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "./card";
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((user, num) => {
+        return (
+          <Card
+            id={robots[num].id}
+            name={robots[num].name}
+            username={robots[num].username}
+            email={robots[num].email}
+          />
+        );
+      })}
+    </div>
+  );
+};
+export default CardList;
---
+++ src/containers/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import CardList from "../components/cardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/scroll";
+import { robots } from "../robots";
+import "../App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchFields: "",
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchFields: event.target.value });
+    console.log(this.state.searchFields);
+  };
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((respond) => respond.json)
+      .then((user) => this.setState({ robot: user }));
+  }
+  render() {
+    const {robots, searchFields} = this.state
+    const filterRobots = robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(searchFields.toLowerCase());
+    });
+    // console.log(filterRobots);
+    return (!robots.length) ?
+      (
+  
+        <div className="loadapp">
+          <h1>Loading...</h1>
+        </div>
+      )
+       :
+      (
+        <div className="app-style">
+          <h1>Robo Friends Gallary</h1>
+          {/* <h1>{this.state.searchFields}</h1> */}
+          <SearchBox onSearchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filterRobots} />
+          </Scroll>
+        </div>
+      );
+    
+  }
+}
+export default App;
